f8cdc7fd26b41f32b7f8db0dbcf2ab4e969c9d65
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-23 21:41:01 -0400

    applying styles and shadow.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Platform } from 'react-native';
 
 export const Header = () => {
   return (
-    <Text>Albums!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Albums!</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    // borderWidth: 3,
+    elevation: 25,
+    height: 120,
+    justifyContent: 'center',
+    paddingTop: 15,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.1,
+        paddingTop: 15
+      },
+      android: {
+        elevation: 10
+      }
+    })
+
+  },
+  text: {
+    fontSize: 20
+  }
+});
 
 export default Header;
